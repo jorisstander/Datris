27e826b22c355627a5e8c0e02892a0724336da4c
jDorrrrian <jessinrodenburg@hoppinger.com>
2020-06-04 23:14:35 +0200

    updated gamecontroller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -23,6 +23,7 @@ public class GameController : MonoBehaviour
     public Transform[,] objects;
     public Dictionary<Vector3Int, GameObject> blocks = new Dictionary<Vector3Int, GameObject>();
     public GameObject[] gameObjects;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,17 +38,46 @@ public class GameController : MonoBehaviour
         blocks.Clear();
         gameObjects = GameObject.FindGameObjectsWithTag("block");
 
-        foreach(GameObject g in gameObjects)
+        foreach (GameObject g in gameObjects)
         {
             Vector3Int v3 = new Vector3Int((int)g.transform.position.x, (int)g.transform.position.y, (int)g.transform.position.z);
             Debug.Log("G:" + g.name + ",   " + v3.ToString());
             blocks.Add(v3, g);
+            checkBlocks(v3);
         }
     }
 
     void checkBlocks(Vector3Int pos)
     {
+        Vector3Int[] directions = {
+            Vector3Int.up,
+            Vector3Int.down,
+            Vector3Int.left,
+            Vector3Int.right
+        };
 
+        foreach(Vector3Int v in directions)
+        {
+            GameObject g = checkBlock(pos, v);
+            if(g)
+            {
+                Debug.Log(g.gameObject.transform.position);
+            }
+        }
+    }
+
+    GameObject checkBlock(Vector3Int pos, Vector3Int where)
+    {
+        GameObject g;
+        blocks.TryGetValue(pos + where, out g);
+        if (g)
+        {
+            return g;
+        }
+        return g;
+        
+
+        
     }
 
 
